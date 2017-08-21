120d6d27a0dadf1dc451b9376f73b1e83fd03d23
wenge1215 <wenge1215@gmail.com>
2017-08-21 17:23:54 +0800

    通过依赖 Kotlin Android Extensions，import kotlinx.android.synthetic.main.*********完成控件的绑定，简化代码

--- app/src/main/java/wenge/com/testkotlin/ui/MainActivity.kt
+++ app/src/main/java/wenge/com/testkotlin/ui/MainActivity.kt
@@ -3,16 +3,18 @@ package wenge.com.testkotlin.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.widget.TextView
 import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*       //使用Kotlin Android Extensions 依赖，省略 查找控件
+
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 import wenge.com.testkotlin.R
 import wenge.com.testkotlin.domian.commands.RequestForecastCommand
 import wenge.com.testkotlin.ui.adapter.RvAdapter
+
 
 class MainActivity : AppCompatActivity(){
 
@@ -35,8 +37,8 @@ class MainActivity : AppCompatActivity(){
         setContentView(R.layout.activity_main)
 //        val rvList = findViewById<RecyclerView>(R.id.rv_list)
 
-        val rvList = find<RecyclerView>(R.id.rv_list)
-        rvList.layoutManager = LinearLayoutManager(this);
+//        val rvList = find<RecyclerView>(R.id.rv_list)
+        fList.layoutManager = LinearLayoutManager(this)
 //        rvList.adapter = RvAdapter(items)
 
         /**
@@ -52,7 +54,8 @@ class MainActivity : AppCompatActivity(){
 //                 * OnItemClickListener：监听器
 //                 */
 ////                rvList.adapter = RvAdapter(forecastList,this@MainActivity)
-                rvList.adapter = RvAdapter(forecastList){toast(it.date)}   //相当于 rvList.setAdapter(new RvAdapter(forecastList))
+
+                fList.adapter = RvAdapter(forecastList){toast(it.date)}   //相当于 rvList.setAdapter(new RvAdapter(forecastList))
             }
         }
     }
--- app/src/main/java/wenge/com/testkotlin/ui/adapter/RvAdapter.kt
+++ app/src/main/java/wenge/com/testkotlin/ui/adapter/RvAdapter.kt
@@ -4,10 +4,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.tt.lvruheng.eyepetizer.utils.ImageLoadUtils
-import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.layout_rv_item.view.*
 import wenge.com.testkotlin.R
 import wenge.com.testkotlin.domian.model.Forecast
 import wenge.com.testkotlin.domian.model.ForecastList
@@ -17,64 +15,32 @@ import wenge.com.testkotlin.domian.model.ForecastList
  * 备注：
  */
 
-//class RvAdapter(val weekForecast: ForecastList, val itemClick: RvAdapter.OnItemClickListener) : RecyclerView.Adapter<RvAdapter.Viewholder>() {
 class RvAdapter(val weekForecast: ForecastList, val itemClick: (Forecast) -> Unit) : RecyclerView.Adapter<RvAdapter.Viewholder>() {
     override fun onBindViewHolder(holder: Viewholder?, position: Int) {
-//        with(weekForecast.dailyForecast[position]) {
-//            holder!!.textView.text="$date - $description - $high/$low"
-//        }
-//        with(weekForecast[position]) {
-//            holder!!.textView.text = "$date - $description - $high/$low"
-//        }
-
         holder!!.bindForecast(weekForecast[position])
-
     }
 
     override fun getItemCount(): Int = weekForecast.size()
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): Viewholder {
-//        return Viewholder(TextView(parent!!.getContext()))
         val view = LayoutInflater.from(parent!!.context).inflate(R.layout.layout_rv_item, parent, false)
         return Viewholder(view, itemClick)
     }
-
-//    class Viewholder(view: View, val itemClick: OnItemClickListener) : RecyclerView.ViewHolder(view) {
     class Viewholder(view: View, val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view) {
-        private val iconView: ImageView
-        private val dateView: TextView
-        private val descriptionView: TextView
-        private val maxTemperatureView: TextView
-        private val minTemperatureView: TextView
-
-        /**
-         * 初始化控件
-         */
-        init {
-            iconView = view.find(R.id.icon)
-            dateView = view.find(R.id.date)
-            descriptionView = view.find(R.id.description)
-            maxTemperatureView = view.find(R.id.maxTemperature)
-            minTemperatureView = view.find(R.id.minTemperature)
-        }
-
         /**
          * 控件与数据绑定
          * @parms:forecast 数据源
          */
         fun bindForecast(forecast: Forecast) {
-            with(forecast) {
-                ImageLoadUtils.display(itemView.context, iconView, iconUrl)
-                dateView.text = date
-                descriptionView.text = description
-                maxTemperatureView.text = "${high.toString()}"
-                minTemperatureView.text = "${low.toString()}"
-                itemView.setOnClickListener { itemClick(forecast) }
+            with(forecast){
+                ImageLoadUtils.display(itemView.context, itemView.icon, iconUrl)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = "${high.toString()}"
+                itemView.minTemperature.text = "${low.toString()}"
+                itemView.setOnClickListener { itemClick(this) }
             }
         }
     }
 
-//    public interface OnItemClickListener {
-//        operator fun invoke(forecast: Forecast)
-//    }
 }
